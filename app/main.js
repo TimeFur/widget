99572a908fd021519ec5b551ba3365ace47ca744
Wayne <djs860403@gmail.com>
2023-02-04 21:39:40 +0800

    Postmessage with extension
    1. In iframe, document are different between with parent, so it cannot
       use drag and drop framework to dragging image.
    2. Using postmessage to send command.

--- app/main.js
+++ app/main.js
@@ -1,7 +1,38 @@
 window.addEventListener('load', () => {
+    var extBtn = document.querySelector("#extensionBtnId")
     var clockEle = document.querySelector('#clockId')
     timerFunc(clockEle)
+
+    extBtn.addEventListener('click', (e) => {
+        console.log("extension click")
+        window.postMessage({ cmd: "FROM_WIDGET_GET_SHOOT_IMAGES" }, "*")
+    })
 })
+
+window.addEventListener('message', (e) => {
+    if (e.data == null || e.data == undefined)
+        return;
+
+    var cmd = e.data.cmd
+    var data = e.data.data
+    switch (cmd) {
+        case "FROM_CONTENT_REPONSE_WIDGET_REQ":
+            getExtensionResponse(cmd, data)
+            break;
+        default:
+            break;
+    }
+})
+
+const getExtensionResponse = (cmd, data) => {
+    switch (cmd) {
+        case "FROM_CONTENT_REPONSE_WIDGET_REQ":
+            var extImg = document.querySelector('#extImgId')
+            extImg.src = data
+            break;
+    }
+    // extImgId
+}
 
 const timerFunc = (ele = null) => {
     if (ele == null) return

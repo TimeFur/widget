4eb05f57931b3b4fc94d778fb6cf9c71bf0f1a0c
Wayne <djs860403@gmail.com>
2023-02-17 23:34:20 +0800

    Add delete button for annotation
    1. While hover into annotation, show up delete button, vice versa.
    2. Del-btn would remove edit, anchor and svg line.

--- app/annotation.js
+++ app/annotation.js
@@ -27,190 +27,22 @@ const createAnnotation = () => {
         wrapper.appendChild(edit)
         wrapper.appendChild(anchor)
         svgWrapper.appendChild(line)
+
     })
-}
-
-const updateImgSrc = (src = "") => {
-    var imgWrapper = document.querySelector('#imgWrapperId')
-    var imgEle = document.querySelector('#imgId')
-    var cropTop = document.querySelector('.img-crop-top')
-    var cropLeft = document.querySelector('.img-crop-left')
-    var cropBottom = document.querySelector('.img-crop-bottom')
-    var cropRight = document.querySelector('.img-crop-right')
-    var registerFlag = (imgEle.getAttribute('src') == "") ? true : false
-
-    imgEle.src = src
-    imgEle.style.userSelect = 'none'
-    var info = {
-        moveFlag: false,
-        offsetLeft: 0,
-        offsetTop: 0,
-        clickShiftLeft: 0,
-        clickShiftTop: 0,
-        clickShiftRight: 0,
-        clickShiftBottom: 0,
-        cropInfo: {
-            cropTopFlag: false,
-            cropLeftFlag: false,
-            cropBottomFlag: false,
-            cropRightFlag: false,
-            imgWrapper: {},
-            imgEle: {}
-        }
-    }
-
-    if (registerFlag == false)
-        return
-    //setting wrapper width and height
-    imgWrapper.style.width = imgEle.getBoundingClientRect().width + 'px'
-    imgWrapper.style.height = imgEle.getBoundingClientRect().height + 'px'
-    imgEle.style.width = imgEle.getBoundingClientRect().width + 'px'
-    imgEle.style.height = imgEle.getBoundingClientRect().height + 'px'
-
-    //event listener
-    imgWrapper.addEventListener('mouseenter', (e) => {
-        var eleList = document.querySelectorAll('.crop-anchor-style')
-        eleList.forEach(ele => {
-            ele.style.opacity = '100%'
-        })
-    })
-    imgWrapper.addEventListener('mouseleave', (e) => {
-        var eleList = document.querySelectorAll('.crop-anchor-style')
-        eleList.forEach(ele => {
-            ele.style.opacity = '0'
-        })
-    })
-    imgWrapper.addEventListener('mousedown', (e) => {
-        info.moveFlag = true
-        info.offsetLeft = imgWrapper.offsetLeft - e.clientX
-        info.offsetTop = imgWrapper.offsetTop - e.clientY
-        info.clickShiftLeft = imgWrapper.getBoundingClientRect().left - e.clientX
-        info.clickShiftTop = imgWrapper.getBoundingClientRect().top - e.clientY
-        info.clickShiftRight = imgWrapper.getBoundingClientRect().right - e.clientX
-        info.clickShiftBottom = imgWrapper.getBoundingClientRect().bottom - e.clientY
-    })
-    document.addEventListener('mouseup', (e) => {
-        info.moveFlag = false;
-        info.cropInfo.cropTopFlag = false;
-        info.cropInfo.cropLeftFlag = false;
-        info.cropInfo.cropBottomFlag = false;
-        info.cropInfo.cropRightFlag = false;
-    })
-    document.addEventListener('mousemove', (e) => {
-        let mx = e.clientX
-        let my = e.clientY
-        const { cropTopFlag, cropLeftFlag, cropRightFlag, cropBottomFlag } = info.cropInfo
-        if (cropTopFlag || cropLeftFlag || cropRightFlag || cropBottomFlag) {
-
-            //crop align vertical
-            const imgWrapperTop = my + info.offsetTop
-            var imgWrapperHeight = info.cropInfo.imgWrapper.bottom - my - info.clickShiftTop
-            const imgEleTop = imgWrapperHeight - info.cropInfo.imgEle.height
-                + (info.cropInfo.imgEle.bottom - info.cropInfo.imgWrapper.bottom)
-
-            const imgWrapperLeft = mx + info.offsetLeft
-            var imgWrapperWidth = info.cropInfo.imgWrapper.right - mx - info.clickShiftLeft
-            const imgEleLeft = imgWrapperWidth - info.cropInfo.imgEle.width
-                + (info.cropInfo.imgEle.right - info.cropInfo.imgWrapper.right)
-
-            //avoid over img range
-            if (cropTopFlag && imgEleTop > 0)
-                return
-            if (cropLeftFlag && imgEleLeft > 0)
-                return
-
-            if (cropTopFlag) {
-                imgWrapper.style.top = imgWrapperTop + 'px'
-                imgWrapper.style.height = `${imgWrapperHeight}px`
-                imgEle.style.top = `${imgEleTop}px`
-            }
-            if (cropLeftFlag) {
-                imgWrapper.style.left = imgWrapperLeft + 'px'
-                imgWrapper.style.width = `${imgWrapperWidth}px`
-                imgEle.style.left = `${imgEleLeft}px`
-            }
-            if (cropRightFlag) {
-                imgWrapperWidth = mx - info.cropInfo.imgWrapper.left + info.clickShiftRight
-                if (imgWrapperWidth + info.cropInfo.imgWrapper.left > info.cropInfo.imgEle.right)
-                    imgWrapperWidth = info.cropInfo.imgEle.right - info.cropInfo.imgWrapper.left
-                imgWrapper.style.width = `${imgWrapperWidth}px`
-            }
-            if (cropBottomFlag) {
-                imgWrapperHeight = my - info.cropInfo.imgWrapper.top + info.clickShiftBottom
-                if (imgWrapperHeight + info.cropInfo.imgWrapper.top > info.cropInfo.imgEle.bottom)
-                    imgWrapperHeight = info.cropInfo.imgEle.bottom - info.cropInfo.imgWrapper.top
-                imgWrapper.style.height = `${imgWrapperHeight}px`
-            }
-            imgEle.style.width = `${info.cropInfo.imgEle.width}px`
-            imgEle.style.height = `${info.cropInfo.imgEle.height}px`
-        } else if (info.moveFlag) {
-            const wrapperLeft = mx + info.offsetLeft
-            const wrapperTop = my + info.offsetTop
-            imgWrapper.style.left = wrapperLeft + 'px'
-            imgWrapper.style.top = wrapperTop + 'px'
-        }
-    })
-    imgWrapper.addEventListener('wheel', function (e) {
-        var preHeight = imgWrapper.getBoundingClientRect().height
-        var preWidth = imgWrapper.getBoundingClientRect().width
-        var ratio = preHeight / preWidth
-
-        //fix height to width ratio
-        var w = preWidth + Math.floor(e.deltaY);
-        var h = ratio * w;
-
-        if (w > 10 && h > 10) {
-            this.style.width = `${w}px`;
-            this.style.height = `${h}px`;
-
-            //after scale image, shift 
-            const wrapperTop = this.offsetTop + (preHeight - h) / 2
-            const wrapperLeft = this.offsetLeft + (preWidth - w) / 2
-            imgWrapper.style.top = wrapperTop + 'px'
-            imgWrapper.style.left = wrapperLeft + 'px'
-
-            //fix relative imgEle position
-            imgEle.style.top = `${imgEle.offsetTop * (h / preHeight)}px`
-            imgEle.style.left = `${imgEle.offsetLeft * (w / preWidth)}px`
-            imgEle.style.width = `${imgEle.getBoundingClientRect().width * (w / preWidth)}px`
-            imgEle.style.height = `${imgEle.getBoundingClientRect().height * (h / preHeight)}px`
-        }
-
-        //avoid background page scroll
-        e.preventDefault();
-    });
-
-    //crop event
-    cropTop.addEventListener('mousedown', (e) => {
-        info.cropInfo.cropTopFlag = true
-        info.cropInfo.imgWrapper = imgWrapper.getBoundingClientRect()
-        info.cropInfo.imgEle = imgEle.getBoundingClientRect()
-    })
-    cropLeft.addEventListener('mousedown', (e) => {
-        info.cropInfo.cropLeftFlag = true
-        info.cropInfo.imgWrapper = imgWrapper.getBoundingClientRect()
-        info.cropInfo.imgEle = imgEle.getBoundingClientRect()
-    })
-    cropBottom.addEventListener('mousedown', (e) => {
-        info.cropInfo.cropBottomFlag = true
-        info.cropInfo.imgWrapper = imgWrapper.getBoundingClientRect()
-        info.cropInfo.imgEle = imgEle.getBoundingClientRect()
-    })
-    cropRight.addEventListener('mousedown', (e) => {
-        info.cropInfo.cropRightFlag = true
-        info.cropInfo.imgWrapper = imgWrapper.getBoundingClientRect()
-        info.cropInfo.imgEle = imgEle.getBoundingClientRect()
-    })
-
-    //disable drag hover
-    imgWrapper.addEventListener('dragstart', (e) => { e.preventDefault() })
 }
 
 const createAnnoContainer = (id = "") => {
     return new Promise((resolve) => {
-        var edit = createEditContainer();
+        var { edit, delBtn } = createEditContainer();
         var line = createSvgLine();
         var anchor = createAnchor()
+
+        //add delete icon
+        delBtn.addEventListener('click', (e) => {
+            anchor.remove()
+            line.remove()
+            edit.remove()
+        })
 
         var annoInfo = {
             editMoveFlag: false,
@@ -281,14 +113,24 @@ const createAnnoContainer = (id = "") => {
             }
         })
 
+        edit.addEventListener('mouseenter', (e) => {
+            delBtn.style.opacity = "100%"
+        })
+        edit.addEventListener('mouseleave', (e) => {
+            delBtn.style.opacity = "0"
+        })
+
         resolve({ edit, anchor, line })
     })
 }
 
-createEditContainer = () => {
+const createEditContainer = () => {
     var editWrapper = document.createElement('div')
     var editTitle = document.createElement('div')
     var editContent = document.createElement('div')
+    var delBtn = document.createElement('div');
+
+    delBtn.className = "anno-del-btn"
 
     editTitle.setAttribute('contenteditable', true)
     editTitle.textContent = "Title"
@@ -298,6 +140,7 @@ createEditContainer = () => {
 
     editWrapper.appendChild(editTitle)
     editWrapper.appendChild(editContent)
+    editWrapper.appendChild(delBtn)
 
     editWrapper.style.position = "absolute"
     editWrapper.style.left = "0px"
@@ -312,9 +155,9 @@ createEditContainer = () => {
     editWrapper.style.zIndex = 5
     editWrapper.style.userSelect = 'none'
 
-    return editWrapper
+    return { edit: editWrapper, delBtn: delBtn }
 }
-createSvgLine = () => {
+const createSvgLine = () => {
     var line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
     line.setAttribute('x1', '0');
     line.setAttribute('y1', '0');
@@ -326,7 +169,7 @@ createSvgLine = () => {
     return line
 }
 
-createAnchor = () => {
+const createAnchor = () => {
     var anchor = document.createElement('div')
     anchor.style.position = 'absolute'
     anchor.style.left = "0px"
@@ -343,7 +186,6 @@ createAnchor = () => {
 
 const AnnoInst = {
     imageEditor: ImageEditor,
-    updateImgSrc: updateImgSrc,
     createAnnotation: createAnnotation,
     clipFunc: clipFunc
 }
